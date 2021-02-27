5da2a4b951180d2a86fde6e76026703d08598c41
Darshaan Devendra Aghicha <dsaghicha211@gmail.com>
2021-02-28 01:59:58 +0530

    Removed requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="CleanUP",
     version=1.0,
     packages=['cleanup'],
-    requires='requirements.txt',
     entry_points={
         'console_scripts': [
             'cleanup = cleanup.__main__:main'
